a113a58fd576deaa4110d2df92efa7be6642ca14
mizamarzes <mizamarzes@gmail.com>
2024-08-29 10:18:28 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-xl font-bold text-red-500'>Hello Talaskdñlsakdlñ</h1>      
+    </>
+  )
+}
+
+export default App
