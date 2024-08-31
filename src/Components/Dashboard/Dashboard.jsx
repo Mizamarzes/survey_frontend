f303e36d887edb3c98699de0c98764ddd9281aed
danielnavasbecerra <danielnavasb14@gmail.com>
2024-08-31 15:29:17 -0500

    feat: :zap: First part of login

---
+++ src/Components/Dashboard/Dashboard.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Dashboard = () => {
+  return (
+    <div>
+      This is a Dashboard Page
+      <br />
+      <a href="/">Log Out</a>
+    </div>
+  );
+};
+
+export default Dashboard;
