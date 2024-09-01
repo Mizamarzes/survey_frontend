86a4ca2a7894b9c70a4c30981b23b1fabcdbb1f2
danielnavasbecerra <danielnavasb14@gmail.com>
2024-09-01 15:05:53 -0500

    feat: :construction: Conect Login With Back

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Login.css";
 import "../../App.css";
-import { Link, NavLink } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import Axios from "axios";
 
 // Import our assets
 import logo from "../../LoginAssets/logo.png";
@@ -12,6 +13,57 @@ import { BsFillShieldLockFill } from "react-icons/bs";
 import { AiOutlineSwapRight } from "react-icons/ai";
 
 const Login = () => {
+  // UseState Hook to store inputs
+  const [loginUsername, setLoginUsername] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const navigateTo = useNavigate();
+
+  //Let us now show the message to the user
+  const [loginStatus, setLoginStatus] = useState("");
+  const [statusHolder, setstatusHolder] = useState("message");
+
+  // Onclick let us get what the user has entered
+  const loginUser = (e) => {
+    e.preventDefault(); // Evita que la página se recargue
+    Axios.post("http://localhost:8080/auth/login", {
+      username: loginUsername, // Cambié Username a username
+      password: loginPassword, // Cambié Password a password
+    })
+      .then((response) => {
+        console.log(response);
+        if (
+          response.data.message ||
+          loginUsername == "" ||
+          loginPassword == ""
+        ) {
+          // If credential don't match
+          navigateTo("/"); // Navigate to the same login page
+          setLoginStatus(`Credetials Don´t Exist!`);
+        } else {
+          // If credential match
+          navigateTo("/dashboard"); // Navigate to the dashboard
+        }
+      })
+      .catch((error) => {
+        console.error("There was an error loging the user:", error);
+      });
+  };
+
+  useEffect(() => {
+    if (loginStatus !== "") {
+      setstatusHolder("showMessage");
+      setTimeout(() => {
+        setstatusHolder("message");
+      }, 4000); // 4 seconds
+    }
+  }, [loginStatus]);
+
+  // Lets clear the form on submit
+  const onSubmit = () => {
+    setLoginUsername("");
+    setLoginPassword("");
+  };
+
   return (
     <div className="loginPage flex">
       <div className="container flex">
@@ -37,14 +89,21 @@ const Login = () => {
             <h3>Welcome Back!</h3>
           </div>
 
-          <form action="" className="form grid">
-            <span className="showMessage">Login Status will go here</span>
+          <form action="" className="form grid" onSubmit={onSubmit}>
+            <span className={statusHolder}>{loginStatus}</span>
 
             <div className="inputDiv">
               <label htmlFor="username">Username</label>
               <div className="input flex">
                 <FaUserShield className="icon" />
-                <input type="text" id="username" placeholder="Enter Username" />
+                <input
+                  type="text"
+                  id="username"
+                  placeholder="Enter Username"
+                  onChange={(event) => {
+                    setLoginUsername(event.target.value);
+                  }}
+                />
               </div>
             </div>
 
@@ -56,11 +115,14 @@ const Login = () => {
                   type="password"
                   id="password"
                   placeholder="Enter Password"
+                  onChange={(event) => {
+                    setLoginPassword(event.target.value);
+                  }}
                 />
               </div>
             </div>
 
-            <button type="submit" className="btn flex">
+            <button type="submit" className="btn flex" onClick={loginUser}>
               <span>Login</span>
               <AiOutlineSwapRight className="icon" />
             </button>
