77d5e5cc40b547189a2fc590dd81f1006852e079
mizamarzes <mizamarzes@gmail.com>
2024-09-03 15:10:32 -0500

    implementacion del outlet y uso del navbar

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import Dashboard from "./Components/Dashboard/Dashboard";
 import Login from "./Components/Login/Login";
 import Register from "./Components/Register/Register";
 import { ToastContainer } from 'react-toastify';
+import UpdateSurvey from "./Components/Survey/pages/UpdateSurvey";
+import CreateSurvey from "./Components/Survey/pages/CreateSurvey";
 
 // Import React react dom
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
@@ -33,6 +35,10 @@ const router = createBrowserRouter([
         <Dashboard />
       </div>
     ),
+    children: [
+      { path: "create-survey", element: <CreateSurvey />},
+      { path: "update-survey", element: <UpdateSurvey />}
+    ],
   },
   /* path: "/",
     element: <Dashboard />,
@@ -42,6 +48,7 @@ const router = createBrowserRouter([
       { path: "login", element: <Login /> },
       { path: "register", element: <Register /> },
     ], */
+  
 ]);
 
 function App() {
---
+++ src/Components/Survey/pages/UpdateSurvey.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SurveyForm from '../SurveyForm'
+
+const UpdateSurvey = () => {
+  return (
+    <div>
+        hola
+    </div>
+  )
+}
+
+export default UpdateSurvey
