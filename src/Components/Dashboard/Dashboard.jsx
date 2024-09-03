93ba688ab592c755dbe87f309d2873e2d67e1209
danielnavasbecerra <danielnavasb14@gmail.com>
2024-09-03 13:10:43 -0500

    feat: :art: Creating Surveys

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,14 +1,23 @@
 import React, { useState } from "react";
 import { Layout, Button, theme } from "antd";
-import Logo from './lib/Logo';
+import Logo from "./lib/Logo";
 import Navbar from "../Navbar/Navbar";
 import "./Dashboard.css";
 import ToggleThemeButton from "./lib/ToggleThemeButton";
+import SurveyForm from "../Survey/SurveyForm";
 
 import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import { Content } from "antd/es/layout/layout";
 
 const { Header, Sider } = Layout;
 function Dashboard() {
+  // surveys
+  const [surveys, setSurveys] = useState([]);
+
+  const addSurvey = (newSurvey) => {
+    setSurveys([...surveys, newSurvey]);
+  };
+
   const [darkTheme, setDarkTheme] = useState(true);
   const [collaped, setCollapsed] = useState(false);
 
@@ -32,6 +41,7 @@ function Dashboard() {
         <Logo />
         <Navbar darkTheme={darkTheme} />
         <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
+        <a href="/">LogOut</a>
       </Sider>
       <Layout>
         <Header
@@ -46,8 +56,13 @@ function Dashboard() {
             onClick={() => setCollapsed(!collaped)}
             icon={collaped ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
           />
-          <a href="/">LogOut</a>
         </Header>
+        <Content>
+          <div className="dashboard">
+            <h1>Surveys Dashboard</h1>
+            <SurveyForm addSurvey={addSurvey} />
+          </div>
+        </Content>
       </Layout>
     </Layout>
   );
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,53 +1,55 @@
-import { Menu } from 'antd';
-import { 
-    HomeOutlined,
-    AppstoreOutlined,
-    AreaChartOutlined,
-    PayCircleOutlined,
-    SettingOutlined,
-    BarsOutlined
-} from '@ant-design/icons';
+// Iconos Recomendados
+// AiFillHome: Para la página de inicio.
+// AiFillFileText: Para la sección de encuestas.
+// BsFillJournalBookmarkFill: Para los capítulos.
+// AiFillQuestionCircle: Para las preguntas.
+// AiFillSetting: Para configuraciones.
+import React from "react";
+import { Link } from "react-router-dom";
+import { Menu } from "antd";
+import {
+  HomeOutlined,
+  FileTextFilled,
+  BookFilled,
+  QuestionCircleFilled,
+  SettingFilled,
+  BarsOutlined,
+} from "@ant-design/icons";
 
 const Navbar = ({ darkTheme }) => {
   return (
-    <Menu 
-        theme={darkTheme ? 'dark' : 'light'} 
-        mode='inline' 
-        className='menu-bar'
+    <Menu
+      theme={darkTheme ? "dark" : "light"}
+      mode="inline"
+      className="menu-bar"
     >
+      <Menu.Item key="home" icon={<HomeOutlined />}>
+        <Link to="/">Home</Link>
+      </Menu.Item>
 
-        <Menu.Item key="home" icon={<HomeOutlined />}>
-            Home
-        </Menu.Item>
+      <Menu.Item key="surveys" icon={<FileTextFilled />}>
+        <Link to="/surveys">Surveys</Link>
+      </Menu.Item>
 
-        <Menu.Item key="activity" icon={<AppstoreOutlined />}>
-            Activity
-        </Menu.Item>
+      <Menu.Item key="chapters" icon={<BookFilled />}>
+        <Link to="/chapters">Chapters</Link>
+      </Menu.Item>
 
-        <Menu.SubMenu 
-            key='subtasks' 
-            icon={<BarsOutlined />}
-            title='Tasks'
-        >
-            <Menu.Item key='task-1'>Task 1</Menu.Item>
-            <Menu.Item key='task-2'>Task 2</Menu.Item>
-            <Menu.Item key='task-3'>Task 3</Menu.Item>
-        </Menu.SubMenu>
+      <Menu.Item key="questions" icon={<QuestionCircleFilled />}>
+        <Link to="/questions">Questions</Link>
+      </Menu.Item>
 
-        <Menu.Item key="progress" icon={<AreaChartOutlined />}>
-            Progress
-        </Menu.Item>
+      <Menu.Item key="settings" icon={<SettingFilled />}>
+        Account Settings
+      </Menu.Item>
 
-        <Menu.Item key="survey" icon={<PayCircleOutlined />}>
-            Survey
-        </Menu.Item>
-
-        <Menu.Item key="settings" icon={<SettingOutlined />}>
-            Settings
-        </Menu.Item>
-
+      {/* <Menu.SubMenu key="subtasks" icon={<BarsOutlined />} title="Tasks">
+        <Menu.Item key="task-1">Task 1</Menu.Item>
+        <Menu.Item key="task-2">Task 2</Menu.Item>
+        <Menu.Item key="task-3">Task 3</Menu.Item>
+      </Menu.SubMenu> */}
     </Menu>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
