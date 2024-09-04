7e96324ecd31256ac1bfc6f50b2a93754819ad0a
mizamarzes <mizamarzes@gmail.com>
2024-09-04 17:05:44 -0500

    Seccion de view survey terminada falta el diseño

--- src/Components/Survey/SurveyList.jsx
+++ src/Components/Survey/SurveyList.jsx
@@ -1,39 +1,32 @@
 import React from 'react'
 import Survey from "./Survey";
-import { getAllSurveys, getContacts } from '../../services/SurveyService';
-import { Link } from 'react-router-dom';
 
-const SurveyList = ({ data }) => {
-
-    console.log(data);
+const SurveyList = ({ data , currentPage, getSurveys }) => {
     
     return (
         <div>
-            {data?.length === 0 && <div>Loading....</div>}
+            {data?.content?.length === 0 && <div>Loading....</div>}
 
             <ul>
-                {data?.length > 0 && data?.map(survey => <Survey dataSurvey={survey} key={survey.id} />)}
+                {data?.content?.length > 0 && data.content.map(survey => <Survey dataSurvey={survey} key={survey.id} />)}
             </ul>
 
-            {/* {data?.length > 0 && data?.totalPages > 1 &&
+            {data?.content?.length > 0 && data?.totalPages > 1 &&
             <div>
-                <a onClick={() => getAllContacts(currentPage - 1)} className={0 === currentPage ? 'disabled' : ''}>&laquo;</a>
 
+                { currentPage === 0 ||
+                <a onClick={() => getSurveys(currentPage - 1)} className={currentPage === 0 ? 'disabled' : ''}>&laquo;</a>
+                }
+                
                 { data && [...Array(data.totalPages).keys()].map((page, index) => 
-                    <a onClick={() => getAllContacts(page)} className={currentPage === page ? 'active' : ''} key={page}>{page + 1}</a>)}
+                    <a onClick={() => getSurveys(page)} className={currentPage === page ? 'active' : ''} key={page}>{page + 1}</a>)}
 
-
-                <a onClick={() => getAllContacts(currentPage + 1)} className={data.totalPages === currentPage + 1 ? 'disabled' : ''}>&raquo;</a>
+                { data.last === true ||
+                <a onClick={() => getSurveys(currentPage + 1)} className={data.totalPages === currentPage + 1 ? 'disabled' : ''}>&raquo;</a>
+                }
             </div>            
-            } */}
+            }
 
-            {/* {data.map(survey => (
-                <div key={survey.id}>
-                    <h2>{survey.id}</h2>
-                    <h2>{survey.name}</h2>
-                    <p>{survey.description}</p>
-                </div>
-            ))} */}
         </div>
     )
 }
