997be1839353d37b737bd75a55d95c82bcc9a4f7
danielnavasbecerra <danielnavasb14@gmail.com>
2024-09-06 13:44:12 -0500

    feat: :art: Working in navbar

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -1,40 +1,20 @@
-import { useState, useRef, useEffect } from "react";
-import { Layout, Button, theme } from "antd";
-import Logo from "./lib/Logo";
+import { Button, Layout } from "antd";
+import { useState } from "react";
+import logo from "../../assets/logo.png";
 import Navbar from "../Navbar/Navbar";
-import ToggleThemeButton from "./lib/ToggleThemeButton";
 import "./Dashboard.css";
 
-import { MenuUnfoldOutlined, MenuFoldOutlined } from "@ant-design/icons";
+import { MenuFoldOutlined, MenuUnfoldOutlined } from "@ant-design/icons";
 
 import { Content } from "antd/es/layout/layout";
 
 // Import `Outlet` from react-router-dom
 import { Outlet } from "react-router-dom";
 
-// Api SurveyService.js
-import { getAllSurveys } from "../../services/SurveyService";
-import { toastError } from "../../services/ToastService/ToastService";
-
 const { Header, Sider } = Layout;
+
 function Dashboard() {
-  // surveys
-
-  const [darkTheme, setDarkTheme] = useState(true);
   const [collaped, setCollapsed] = useState(false);
-
-  const modalRef = useRef();
-  const fileRef = useRef();
-  const [data, setData] = useState({});
-  const [currentPage, setCurrentPage] = useState(0);
-
-  const toggleTheme = () => {
-    setDarkTheme(!darkTheme);
-  };
-
-  const {
-    token: { colorBgContainer },
-  } = theme.useToken();
 
   return (
     <Layout>
@@ -42,21 +22,20 @@ function Dashboard() {
         collapsed={collaped}
         collapsible
         trigger={null}
-        theme={darkTheme ? "dark" : "light"}
-        className="sidebar"
+        theme="light"
+        className="text-black"
       >
-        <Logo />
-        <Navbar darkTheme={darkTheme} />
-        <ToggleThemeButton darkTheme={darkTheme} toggleTheme={toggleTheme} />
+        <div className="flex items-center justify-center w-full pt-5">
+          <img className="p-3 h-20 w-20" src={logo} alt="Logo Image" />
+        </div>
+
+        <Navbar />
+
         <a href="/">LogOut</a>
       </Sider>
+
       <Layout>
-        <Header
-          style={{
-            padding: 0,
-            background: colorBgContainer,
-          }}
-        >
+        <Header className="bg-gray-200 p-0">
           <Button
             type="text"
             className="toggle"
@@ -66,9 +45,7 @@ function Dashboard() {
         </Header>
 
         <Content className="flex items-start justify-center bg-gray-200">
-          {/* <div className="bg-white shadow-md rounded-lg p-8 flex flex-col lg:flex-row"> */}
-            <Outlet />
-          {/* </div> */}
+          <Outlet />
         </Content>
       </Layout>
     </Layout>
--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,45 +1,45 @@
-import { Menu } from 'antd';
+import { Menu } from "antd";
 import {
-    HomeOutlined,
-    AppstoreOutlined,
-    AreaChartOutlined,
-} from '@ant-design/icons';
+  HomeOutlined,
+  AppstoreOutlined,
+  AreaChartOutlined,
+} from "@ant-design/icons";
 
-import './Navbar.css';
+import "./Navbar.css";
 
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
-const Navbar = ({ darkTheme }) => {
-    const menuItems = [
-        {
-            key: 'home',
-            icon: <HomeOutlined />,
-            label: <Link to="home">Home</Link>
-        },
-        {
-            key: 'viewsurveys',
-            icon: <HomeOutlined />,
-            label: <Link to="view-survey">View Survey</Link>
-        },
-        {
-            key: 'createsurvey',
-            icon: <AppstoreOutlined />,
-            label: <Link to="create-survey">Create Survey</Link>
-        },
-        {
-            key: 'updatesurvey',
-            icon: <AreaChartOutlined />,
-            label: <Link to="update-survey">Update Survey</Link>
-        },
-    ];
-    return (
-        <Menu
-            theme={darkTheme ? 'dark' : 'light'}
-            mode='inline'
-            className='menu-bar'
-            items={menuItems} // Usar `items` en lugar de `children`
-        />
-    )
-}
+const Navbar = () => {
+  const menuItems = [
+    {
+      key: "home",
+      icon: <HomeOutlined />,
+      label: <Link to="home">Home</Link>,
+    },
+    {
+      key: "viewsurveys",
+      icon: <HomeOutlined />,
+      label: <Link to="view-survey">View Survey</Link>,
+    },
+    {
+      key: "createsurvey",
+      icon: <AppstoreOutlined />,
+      label: <Link to="create-survey">Create Survey</Link>,
+    },
+    {
+      key: "updatesurvey",
+      icon: <AreaChartOutlined />,
+      label: <Link to="update-survey">Update Survey</Link>,
+    },
+  ];
+  return (
+    <Menu
+      theme="light"
+      mode="inline"
+      className="menu-bar"
+      items={menuItems} // Usar `items` en lugar de `children`
+    />
+  );
+};
 
 export default Navbar;
